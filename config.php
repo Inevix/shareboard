2dd936948726109f96cb63a8bd02beaa559b6de9
Serhii Chernenko <serhii.c@overdose.digital>
2020-05-17 17:38:43 +0300

    Removed original config file. Added sample config file

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'oop');
-
-define('ROOT_PATH', '/');
-define('ROOT_URL', 'http://oop.loc/');
